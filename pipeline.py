0a55300c4b5e60c33f3f26a229f129091b8c91b4
vigilanti <hasheersheikh@gmail.com>
2019-07-15 21:54:11 +0530

    files changed

--- pipeline.py
+++ pipeline.py
@@ -18,7 +18,7 @@ def get_study_level_data(study_type):
     study_data = {}
     study_label = {'positive': 1, 'negative': 0}
     for phase in data_cat:
-        BASE_DIR = '/gdive/My\ Drive\MURA-v1.0/%s/%s/' % (phase, study_type)
+        BASE_DIR = '/gdive/My\ Drive/MURA-v1.0/%s/%s/' % (phase, study_type)
         patients = list(os.walk(BASE_DIR))[0][1] # list of patient folder names
         study_data[phase] = pd.DataFrame(columns=['Path', 'Count', 'Label'])
         i = 0
